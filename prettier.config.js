8df57d1f94cbb64d3bed37b8a00be5364ddc1e36
Christopher Brown <chbphone55@gmail.com>
2019-05-04 13:58:16 -0600

    prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  rangeStart: null,
+  rangeEnd: null,
+  filePath: ''
+}
