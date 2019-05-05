3fe7cb0faa839f9ba7294ee4d23f47a8985ae224
Christopher Brown <chbphone55@gmail.com>
2019-05-04 22:10:56 -0600

    add movielist to app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import NewMovie from './components/NewMovie'
 
 import './App.css'
+import MovieList from './components/MovieList'
 
 function App() {
   return (
     <main className="min-h-screen App">
+      <MovieList />
       <NewMovie />
     </main>
   )
