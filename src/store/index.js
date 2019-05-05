f3a3ffbc4892df8a42ae0d8fcb37529215dcc20f
Christopher Brown <chbphone55@gmail.com>
2019-05-04 21:51:04 -0600

    use redux

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux'
+import { storage } from 'kv-storage-polyfill'
+
+const types = {
+  ADD_MOVIE: 0,
+  REMOVE_MOVIE: 1,
+  EDIT_MOVIE: 2
+}
+
+const initialState = []
+function movieReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.ADD_MOVIE:
+      return state.push(action.payload)
+    case types.REMOVE_MOVIE:
+      return [...state.slice(0, action.payload), ...state.slice(action.payload)]
+    case types.EDIT_MOVIE:
+      return [
+        ...state.slice(0, action.payload.index),
+        action.payload.movie,
+        ...state.slice(action.payload.index)
+      ]
+    default:
+      return state
+  }
+}
+
+const store = createStore(movieReducer)
+
+// updates kv-storage with new value on change
+store.subscribe(() => {
+  const state = store.getState()
+  storage.set('movies', state)
+})
+
+export default store
