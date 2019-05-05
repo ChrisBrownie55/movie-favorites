a7353570d06538fdc01e42aaa43ebc56c7d949af
Christopher Brown <chbphone55@gmail.com>
2019-05-05 12:14:12 -0600

    Updates

---
+++ precache-manifest.5c0b19a18501f2fdfaac233a45dba1c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f8514332964c1508fe9316d0860184",
+    "url": "/movie-favorites/index.html"
+  },
+  {
+    "revision": "d3dc9f304a48877b0650",
+    "url": "/movie-favorites/static/css/main.9e5554a8.chunk.css"
+  },
+  {
+    "revision": "c91531ad104f320effd3",
+    "url": "/movie-favorites/static/js/2.665509b3.chunk.js"
+  },
+  {
+    "revision": "d3dc9f304a48877b0650",
+    "url": "/movie-favorites/static/js/main.87ae998d.chunk.js"
+  },
+  {
+    "revision": "847d9c25a0386d9b768b",
+    "url": "/movie-favorites/static/js/runtime~main.6b0a500b.js"
+  },
+  {
+    "revision": "f470cf04e982d135b4ed33886bd56168",
+    "url": "/movie-favorites/static/media/movie-items.f470cf04.svg"
+  }
+]);
